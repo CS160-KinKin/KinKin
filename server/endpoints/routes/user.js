27a792032dd9c19a7459d9f89b876c023c9e08e2
isita <isita.bagayatkar@gmail.com>
2022-04-07 13:13:05 -0700

    wip

--- server/endpoints/routes/user.js
+++ server/endpoints/routes/user.js
@@ -4,3 +4,5 @@ const router = express.Router();
 const axios = require('axios');
 
 const User = require('../models/User');
+
+const { OK, BAD_REQUEST, UNAUTHORIZED, FORBIDDEN, NOT_FOUND, CONFLICT } = require('../../util/constants').STATUS_CODES;
