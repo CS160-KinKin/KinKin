819ecad2a2879e5762747bfbb47737a400ac8786
Josh <joshualawson8@gmail.com>
2022-04-06 21:28:39 -0700

    google login frontend done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import {NavLink} from "react-router-dom";
 import {Footer, Navigation} from "./index";
 import GoogleLogin from "react-google-login";
 
 function Login() {
-    return (
+    const [loginData, setLoginData] = useState(
+        localStorage.getItem('loginData')
+        ? JSON.parse(localStorage.getItem('loginData'))
+        : null
+    );
+    const handleFailure = (result) => {
+        console.log(result);
+    }
+    const handleLogin = async (googleData) => {
+        const res = await fetch('/api/google-login', {
+            method: 'POST',
+            body: JSON.stringify({
+                token: googleData.tokenId,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
 
+        const data = await res.json();
+        setLoginData(data);
+        localStorage.setItem('loginData', JSON.stringify(data));
+
+    }
+    const handleLogout = () => {
+      localStorage.removeItem('loginData');
+      setLoginData(null);
+    }
+    return (
         <div>
             <Navigation />
-                <GoogleLogin
-                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                    buttonText={"Log in with Google"}
-                    onSuccess={handleLogin}
-                >
-                </GoogleLogin>
+            <div class = "center">
+                {
+                    loginData ? (
+                        <div>
+                        <h3>logged in</h3>
+                        <button onClick={handleLogout}>logout</button>
+                        </div>
+                    ) : (
+                    <GoogleLogin
+                        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                        buttonText="Log in with Google"
+                        onSuccess={handleLogin}
+                        onFailure={handleFailure}
+                        cookiePolicy={'single_host_origin'}
+                    >
+                    </GoogleLogin>
+                    )}
+            </div>
             <Footer />
 
         </div>
