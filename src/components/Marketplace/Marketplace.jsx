8665ce59928aa126e0274f44e114eda9d975fc7a
Christina <thechristinang@gmail.com>
2022-04-07 23:56:43 -0700

    changed "class=" to "className="

--- src/components/Marketplace/Marketplace.jsx
+++ src/components/Marketplace/Marketplace.jsx
@@ -14,17 +14,17 @@ function Marketplace() {
     return (
       <div>
         <Navigation />
-        <div class="container">
-          <div class="row align-items-center my-5">
-            <div class="col-lg-7">
+        <div className="container">
+          <div className="row align-items-center my-5">
+            <div className="col-lg-7">
               <img
-                class="img-fluid rounded mb-4 mb-lg-0"
+                className="img-fluid rounded mb-4 mb-lg-0"
                 src="http://placehold.it/900x400"
                 alt=""
               />
             </div>
-            <div class="col-lg-5">
-              <h1 class="font-weight-light">Marketplace</h1>
+            <div className="col-lg-5">
+              <h1 className="font-weight-light">Marketplace</h1>
               <h4> List of suggested PT's for you:</h4>
               <PTProfile />
               <button onClick={sendRequest}>Request</button>
