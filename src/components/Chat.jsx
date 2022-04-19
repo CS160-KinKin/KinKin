6fd9c30649034205853c5b77f4ff17d390526984
Eunice Oh <euniceyoh@gmail.com>
2022-04-18 18:34:31 -0700

    attempted to add testing

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,3 @@
-// user logs into sendbird
-
-// APP ID: 45391E28-BD64-49C5-89A5-C14E1A5E5A0C
-// User ID: user1
-// Nickname: eunice 
-
-// User can select a PT they are interested in 
-
 import { SendBirdProvider, withSendBird } from "sendbird-uikit";
 
 import {App as SendBirdApp} from "sendbird-uikit";
@@ -16,9 +8,6 @@ import styles from './chat.module.css'
 import React from "react";
 import {Navigation} from "./index";
 
-var YOUR_APP_ID = "45391E28-BD64-49C5-89A5-C14E1A5E5A0C"
-var USER_ID = "user1" // pass in correct USER_ID based on login 
-
 const ChatApp = (props) => {
     console.log(props.user)
     return (
@@ -26,7 +15,7 @@ const ChatApp = (props) => {
         <Navigation />
         <div className={styles.App}>
             <SendBirdApp 
-                appId={YOUR_APP_ID} 
+                appId={process.env.REACT_APP_SENDBIRD_ID} 
                 userId={props.user.email}
                 nickname={props.user.name}
                 profileUrl={props.user.picture}
