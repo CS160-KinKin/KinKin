7885c7bd70fb59d98d6ccaf8396810b1aaa24971
Josh <joshualawson8@gmail.com>
2022-04-21 21:56:37 -0700

    add back old test

---
+++ src/components/__tests__/chat.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import {render, screen } from '@testing-library/react'
+import Chat from '../Chat'
+Enzyme.configure({ adapter: new Adapter() });
+
+// meant to test the component but faced issues running 
+
+// describe('Chat', () => {
+//     test('render chat component', () => {
+//         const wrapper = shallow(<Chat/>)
+//         expect(wrapper.exists()).toBe(true);
+//     })
+// })
