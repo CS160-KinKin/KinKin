0f089e45ca97b14e88fa893e3b26e42db271fdef
isabellapiz <isabellapiziali@yahoo.com>
2022-04-21 17:05:54 -0700

    unfinished

---
+++ src/components/Marketplace/Marketplace.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {Navigation} from "../index";
+import PTProfile from "../Profile/PTProfile";
+
+function Marketplace() {
+
+    function sendRequest() {
+        try {
+            let result = await addRequest(this.props.user.token);
+            alert("Request sent to PT.");
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    function sendMessage() {
+        alert("Request sent to PT.");
+    }
+
+    return (
+      <div>
+        <Navigation />
+        <div className="container">
+          <div className="row align-items-center my-5">
+            <div className="col-lg-7">
+              <img
+                className="img-fluid rounded mb-4 mb-lg-0"
+                src="http://placehold.it/900x400"
+                alt=""
+              />
+            </div>
+            <div className="col-lg-5">
+              <h1 className="font-weight-light">Marketplace</h1>
+              <h4> List of suggested PT's for you:</h4>
+              <PTProfile />
+              <button onClick={sendRequest}>Request</button>
+              <button onClick={sendMessage}>Message</button>
+              <br /><br />
+              <PTProfile />
+              <button onClick={sendRequest}>Request</button>
+              <button onClick={sendMessage}>Message</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Marketplace;
