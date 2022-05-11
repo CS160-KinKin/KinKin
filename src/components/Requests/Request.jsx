0bc705e232f9d9c5e82013c10a990f05da29a453
charlotte-zhuang <charlotte.zhuang@sjsu.edu>
2022-05-11 00:28:00 -0700

    add client support to components/Requests

--- src/components/Requests/Request.jsx
+++ src/components/Requests/Request.jsx
@@ -2,25 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import RequestComponent from './RequestComponent';
 import { Footer, Navigation } from '../index';
-import { getRequests } from '../../util/pt';
+import ptUtil from '../../util/pt';
+import clientUtil from '../../util/client';
 
 const Request = (props) => {
+  const { user, type } = props;
   const [requests, setRequests] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
-    try {
-      getRequests(props.user.token).then((response) => setRequests(response));
-    } catch (err) {
-      console.error(err.message);
-    }
-  }, [props.user.token]);
+    const getRequests =
+      type === 'pt'
+        ? ptUtil.getRequests
+        : type === 'client'
+        ? clientUtil.getRequests
+        : () => {
+            throw Error(`invalid type: ${type}`);
+          };
+
+    getRequests(user.token)
+      .then((response) => setRequests(response))
+      .catch((err) =>
+        console.error(`Error in Request.useEffect: ${err.message}`)
+      );
+  }, [user.token, type]);
 
   return (
     <>
       <Navigation {...props} />
       {requests.map((request) => (
-        <RequestComponent user={props.user} client={request} />
+        <RequestComponent
+          user={props.user}
+          other={request}
+          ptSide={type === 'pt'}
+        />
       ))}
       <button className='btn btn-light' onClick={() => navigate(-1)}>
         Back
